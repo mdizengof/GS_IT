2ac758239bff5ba3b9ea5aabb0e3145405c0d198
mark dizengoff <markd@rion.com>
2012-08-26 07:37:11 +0300

    commit

---
+++ app/view/Main.js
@@ -0,0 +1,67 @@
+﻿Ext.define("GS.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'הביתה!',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Rion Android first application'
+                },
+
+                html: [
+                    " Wow! It's my Boss!",
+                    " <a target='_blank' href=\"resources/images/shaked.jpg\">BOSS foto </a>  ",
+                    
+                ].join("")
+            },
+            {
+                title: 'שקד',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'My lovely BOSS'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://www.rion.com',
+                        posterUrl: 'http://localhost/GS/resources/images/shaked.jpg'
+                    }
+                ]
+            }
+            ,
+             {
+                 title: 'נתן',
+                 iconCls: 'action',
+
+                 items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: ' Collega'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://www.rion.com',
+                        posterUrl: 'http://localhost/GS/resources/images/natik.jpg'
+                    }
+                ]
+             }
+        ]
+    }
+});
